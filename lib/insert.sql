67a3d71dde10c36ae8952b568e324fc203794380
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-09 15:08:27 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,13 +3,14 @@ INSERT INTO bears (name, age, gender, color, temperament,alive)
     16, 
     "male",
     "brown",
-    ,1);
+    1);
 INSERT INTO bears (name, age, gender, color, temperament,alive) 
     VALUES ("Rowdy",
     10, 
     "male",
-    "black",
-    , ,0);
+    "black", 
+    "friendly", 
+    0);
 INSERT INTO bears (name, age, gender, color, temperament,alive) 
     VALUES ("Tabitha",
     5,
@@ -22,20 +23,23 @@ INSERT INTO bears (name, age, gender, color, temperament,alive)
     16, 
     "male",
     "brown",
-    , ,1);
+    "friendly", 
+    1);
 INSERT INTO bears (name, age, gender, color, temperament,alive) 
     VALUES ("Melissa",
     7, 
     "female",
     "black",
-    , ,1);
+    "friendly", 
+    1);
     
 INSERT INTO bears (name, age, gender, color, temperament,alive) 
     VALUES ("Grinch",
     2, 
     "male",
     "brown",
-    , ,1);
+    "friendly", 
+    1);
 INSERT INTO bears (name, age, gender, color, temperament,alive) 
     VALUES ("Wendy",
     20, 
